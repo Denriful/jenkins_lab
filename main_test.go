85b3a42e40facbb14986774545f8f054f9bb6129
Denis Sulgin <sulgin.ds@med54.ru>
2020-04-27 08:53:37 +0700

    add example unit test

--- main_test.go
+++ main_test.go
@@ -1,20 +1,23 @@
 package main
 
-import (
-	"log"
-	"net/http"
-)
+import "testing"
 
-type Server struct{}
+func TestSum(t *testing.T) {
+	tables := []struct {
+		x int
+		y int
+		n int
+	}{
+		{1, 1, 2},
+		{1, 2, 3},
+		{2, 2, 4},
+		{5, 2, 7},
+	}
 
-func (s *Server) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	w.WriteHeader(http.StatusOK)
-	w.Header().Set("Content-Type", "application/json")
-	w.Write([]byte(`{"message": "hello world"}`))
-}
-
-func main() {
-	s := &Server{}
-	http.Handle("/", s)
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	for _, table := range tables {
+		total := Sum(table.x, table.y)
+		if total != table.n {
+			t.Errorf("Sum of (%d+%d) was incorrect, got: %d, want: %d.", table.x, table.y, total, table.n)
+		}
+	}
 }
